8e9a05aa8c1e437a8282c666463e1a9cd4ba77f7
Yanhua <bfone12@gmail.com>
2018-03-02 22:17:27 -0800

    init add

--- DockerRestAPI/laptop/api.py
+++ DockerRestAPI/laptop/api.py
@@ -1,25 +1,189 @@
 # Laptop Service
-
-from flask import Flask
+import os
+import pymongo
+from flask import Flask, request
 from flask_restful import Resource, Api
+from pymongo import MongoClient
+
 
 # Instantiate the app
 app = Flask(__name__)
 api = Api(app)
 
-class Laptop(Resource):
+client = MongoClient("db", 27017)
+db = client.tododb
+
+
+#############################################
+
+class ListAll(Resource):
     def get(self):
-        return {
-            'Laptops': ['Mac OS', 'Dell', 
-            'Windozzee',
-	    'Yet another laptop!',
-	    'Yet yet another laptop!'
-            ]
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+
+        result = { 'open_time': [item["open_times"] for item in i],
+        'close_time': [item["close_times"] for item in i],
+        'km': [item["km"] for item in i]
         }
+        return result
+
+
+class ListAllJson(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+        
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        doc = []
+        for item in i:
+            item_doc = {
+                'km': item["km"],
+                'open_time' : item["open_times"],
+                'close_time': item["close_times"]
+            }
+            doc.append(item_doc)
+        return doc
+
+
+class ListAllCsv(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+        
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+
+        result=[]
+        
+        for item in i:
+            temp = item["km"] + ", " + item["open_times"] + ", " + item["close_times"]
+            result.append(temp)
+            
+        return result
+
+
+
+###################################################
+
+
+class ListOpenOnly(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result = { 'open_time': [item["open_times"] for item in i]}
+        return result
+
+class ListOpenOnlyJson(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result = []
+        for item in i:
+            item_doc = {
+                'open_time' : item["open_times"]
+            }
+            result.append(item_doc)
+
+        return result
+
+class ListOpenOnlyCsv(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+    
+        item_list = db.tododb.find().sort("open_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result=[]
+        
+        for item in i:
+            temp = item["open_times"]
+            result.append(temp)
+        return result
+
+
+#################################
+
+
+class ListCloseOnly(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+        item_list = db.tododb.find().sort("close_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result = { 'close_time': [item["close_times"] for item in i]}
+        return result
+
+
+class ListCloseOnlyJson(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+
+        item_list = db.tododb.find().sort("close_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result = []
+        for item in i:
+            item_doc = {
+                'close_time' : item["close_times"]
+            }
+            result.append(item_doc)
+        return result
+
+
+class ListCloseOnlyCsv(Resource):
+    def get(self):
+        top = request.args.get("top")
+        if top == None:
+            top = 100
+
+        item_list = db.tododb.find().sort("close_times", pymongo.ASCENDING).limit(int(top))
+        i = [item for item in item_list]
+        
+        result=[]
+        
+        for item in i:
+            temp = item["close_times"]
+            result.append(temp)
+
+        return result
+
 
 # Create routes
-# Another way, without decorators
-api.add_resource(Laptop, '/')
+
+api.add_resource(ListAll, '/listAll')
+api.add_resource(ListAllJson, '/listAllJson')
+api.add_resource(ListAllCsv, '/listAllCsv')
+
+api.add_resource(ListOpenOnly, '/listOpenOnly')
+api.add_resource(ListOpenOnlyJson, '/listOpenOnlyJson')
+api.add_resource(ListOpenOnlyCsv, '/listOpenOnlyCsv')
+
+api.add_resource(ListCloseOnly, '/listCloseOnly')
+api.add_resource(ListCloseOnlyJson, '/listCloseOnlyJson')
+api.add_resource(ListCloseOnlyCsv, '/listCloseOnlyCsv')
+
 
 # Run the application
 if __name__ == '__main__':
--- DockerRestAPI/website/index.php
+++ DockerRestAPI/website/index.php
@@ -2,16 +2,132 @@
     <head>
         <title>CIS 322 REST-api demo: Laptop list</title>
     </head>
-
+    <style>
+    ul {
+        list-style-type:none;
+    }
+    </style>
     <body>
-        <h1>List of laptops</h1>
+        <h2>List of All</h2>
+
+         <ul >
+            <?php
+            $json = file_get_contents('http://laptop-service/listAll');
+            $obj = json_decode($json);
+            $space = "<br>";
+
+            foreach ($obj as $l) {
+                echo $space;
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }              
+            }
+            ?>  
+            
+        </ul>
+
+        <h2>List of All Json</h2>
         <ul>
             <?php
-            $json = file_get_contents('http://laptop-service/');
+            $json = file_get_contents('http://laptop-service/listAllJson');
             $obj = json_decode($json);
-	          $laptops = $obj->Laptops;
-            foreach ($laptops as $l) {
+            $space = "<br>";
+            foreach ($obj as $l) {
+                echo $space;
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }               
+            }
+            ?>
+        </ul>
+
+        <h2>List of All Csv</h2>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listAllCsv');
+            $obj = json_decode($json);
+
+            $space = "&nbsp";
+            foreach ($obj as $l) {
+                echo "<li>$l</li>";      
+                     
+            }
+            ?>
+        </ul>
+
+        <h2>List Open Only</h2>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnly');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {     
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }      
+            }
+            ?>
+        </ul>
+
+        <h2>List Open Only Json</h2>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnlyJson');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {     
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }               
+            }
+            ?>
+        </ul>
+
+        <h2>List Open Only CSV</h2>
+         <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listOpenOnlyCsv');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {     
                 echo "<li>$l</li>";
+                             
+            }
+            ?>
+        </ul>
+        <h2>List Close Only</h2>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listCloseOnly');
+            $obj = json_decode($json);
+            $obj->All;
+            $space = "<br>";
+            foreach ($obj as $l) {     
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }               
+            }
+            ?>
+        </ul>
+
+        <h2>List Close Only Json</h2>
+        <ul>
+            <?php
+            $json = file_get_contents('http://laptop-service/listCloseOnlyJson');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {     
+                foreach ($l as $a){
+                    echo "<li>$a</li>";
+                }               
+            }
+            ?>
+
+        </ul>
+        <h2>List Close Only Csv</h2>
+         <ul>           
+            <?php
+            $json = file_get_contents('http://laptop-service/listCloseOnlyCsv');
+            $obj = json_decode($json);
+            foreach ($obj as $l) {     
+                
+                echo "<li>$l</li>";                           
             }
             ?>
         </ul>
